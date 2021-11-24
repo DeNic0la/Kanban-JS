c403e4ca4b95eeb4a90bdc4a5eff0166491070a0
nicola <nicola@fioretti.info>
2021-11-24 21:03:40 +0100

    Le Api now does stuff.

--- backend/backend.js
+++ backend/backend.js
@@ -1,7 +1,68 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+import { helpers, Router } from 'https://deno.land/x/oak/mod.ts';
 const backendRouter = new Router({prefix:'/api'});
+let columns = {
+    1:{
+        id:1,
+        name: "TODO",
+        cards:[],
+    },
+    2:{
+        id:2,
+        name: "Work in Progress",
+        cards:[],
+    },
+    3:{
+        id:3,
+        name: "Done",
+        cards:[],
+    }
+};
+let cardCount = 0;
+function getGeneratedCard(){
+    return {id:cardCount++,name:"MeiVeryCoolCard"}
+}
+for (let columnsKey in columns) {
+    let cardi = [];
+    cardi.push(getGeneratedCard());
+    cardi.push(getGeneratedCard());
+    cardi.push(getGeneratedCard());
+    columns[columnsKey].cards = cardi
+}
+function getCards(){
+    let cards = {};
+    for (let columnsKey in columns) {
+        let leCards = columns[columnsKey].cards;
+        for (let leCardsKey in leCards) {
+            let leCard = leCards[leCardsKey];
+            cards[leCard.id] = leCard;
+        }
+    }
+    return cards;
+}
 backendRouter.get("/",(context)=>{
     context.response.body = "Hallo BackEnd";
+});
+backendRouter.get("/column/", (context)=> {
+    context.response.body = columns;
+});
+backendRouter.get("/column/:columnId", (context)=> {
+    const { columnId } = helpers.getQuery(context, { mergeParams: true });
+    if (columns.hasOwnProperty(columnId)){
+        context.response.body = columns[columnId];
+    }
+    else {
+        context.response.code = 404;
+    }
+});
+backendRouter.get("/card/", (context)=> {
+    context.response.body = getCards();
+});
+backendRouter.get("/card/:cardId", (context)=> {
+    const { cardId } = helpers.getQuery(context, { mergeParams: true });
+    let cards = getCards();
+    if (cards.hasOwnProperty(cardId)) {
+        context.response.body = cards[cardId]
+    }
 });
 
 
