df737ae81155b62dce992f9fe67d3c5f80b6d6fb
Laurin-dev <71706493+Laurin-dev@users.noreply.github.com>
2021-12-12 19:46:34 +0100

    revert frontend.js oopsie

--- frontend/views/frontend.js
+++ frontend/views/frontend.js
@@ -1,9 +1,26 @@
-'use strict';
+'use strict'
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
+import { renderFile, configure } from "https://deno.land/x/eta@v1.12.3/mod.ts"
+import {send} from "https://deno.land/x/oak@v6.3.1/mod.ts";
 
-var items = [1, 2, 3];
-var target = document.querySelector(".bi.bj .bi.bj");
-var template = "<div><label class=\"bo bp\"><input type=\"checkbox\" name=\"addees[~id~]\" value=\"~id~\">Test ~id~</label></div>";
+//Path to the View Files
+const viewPath = `${Deno.cwd()}/frontend/views/`
+configure({
+    views: viewPath
+})
+const content = {cols:[
+        {id:1,name:"Todo",cards:[]},
+        {id:2,name:"Work in Progress",cards:[]},
+        {id:3,name:"Very Done",cards:[]},
+    ]};
 
-items.forEach(function(item) {
-    target.insertAdjacentHTML("beforeend", template.replace(/~id~/g, item));
-});
+const frontendFiles = context => send(
+    context,
+    context.request.url.pathname,
+    {
+        root: `${Deno.cwd()}/frontend`,
+        index: "index.html"
+    });
+
+
+export {frontendFiles};
